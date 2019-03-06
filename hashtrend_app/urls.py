c2dc701504d6a23a216fd16906d506836661a3b3
emas89 <emanuele.pellegrini89@gmail.com>
2019-03-06 12:32:19 +0100

    Launching to production

---
+++ hashtrend_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+
+urlpatterns = [
+	url(r'^search/$', views.search, name='search'),
+	url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+	url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+	url(r'^register/$', views.sign_up, name='sign_up'),
+	url(r'^account/$', views.account, name='account'),
+]
